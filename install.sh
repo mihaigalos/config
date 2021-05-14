f02cbe0caaf241409b10ab5d156caacafd2388b6
Mihai Galos <mihaigalos@gmail.com>
2021-05-14 12:58:33 +0200

    Use custom 60s and i3bar format (json)

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #! /bin/sh
 
+sudo rm /etc/i3status.conf
+
 ln -s $(realpath .xprofile) ~
 ln -s $(realpath i3_config) ~/.config/i3/config
+ln -s $(realpath i3status.conf) /etc/i3status.conf
 ln -s $(realpath terminator_config) ~/.config/terminator/config
+
