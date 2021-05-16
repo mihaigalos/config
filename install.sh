722726dd52a397b95991b94f5da01f05ca03b50d
Mihai Galos <m.galos@viewpointsystem.com>
2021-05-16 14:06:14 +0200

    Only lock when no sound, avoid lock during youtube

--- install.sh
+++ install.sh
@@ -2,8 +2,11 @@
 
 sudo rm /etc/i3status.conf
 
+mkdir -p ~/.local/bin
+
 ln -s $(realpath .xprofile) ~
 ln -s $(realpath i3_config) ~/.config/i3/config
 ln -s $(realpath i3status.conf) /etc/i3status.conf
 ln -s $(realpath terminator_config) ~/.config/terminator/config
 
+cp lock_when_no_sound ~/.local/bin 
